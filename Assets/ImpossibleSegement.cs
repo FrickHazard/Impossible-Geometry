9746614e7bac73d5c19d64e762b72e7278a80fa7
ekeldevious <ender@shpazzle.com>
2017-10-15 17:30:18 -0400

    Worked on COnnected segment display

---
+++ Assets/ImpossibleSegement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ImpossibleSegementType {
+    Spacer,
+    Caster,
+};
+
+
+public class ImpossibleSegement {
+
+    public Vector3 Start;
+
+    public Vector3 End;
+
+    public ImpossibleSegement StartSegment = null;
+
+    public ImpossibleSegement EndSegment = null;
+
+    public ImpossibleSegement(Vector3 start, Vector3 end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public void CastInterection(Camera camera)
+    {
+        
+    }
+
+}
---
+++ Assets/ImpossibleSegmentRenderer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ImpossibleSegmentRenderer : MonoBehaviour {
+
+    public Vector3 StartPoint;
+    public Vector3 EndPoint;
+    public ImpossibleSegementType SegmantType;
+
+    public Material SpacerMaterial;
+    public Material CastMaterial;
+
+    private MeshFilter filter;
+    private MeshRenderer meshRenderer;
+    private Mesh mesh;
+    private ImpossibleSegement segment;
+
+    void Start () {
+        mesh = new Mesh();
+        filter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        BuildMesh();
+    }
+
+    void BuildMesh()
+    {
+       
+        if (filter != null && meshRenderer && SpacerMaterial && CastMaterial)
+        {
+            mesh.Clear();           
+            Vector3 forward = -Vector3.Normalize(EndPoint - StartPoint);
+            Quaternion lookRotation = Quaternion.LookRotation(forward);
+            Vector3 up = lookRotation * -Vector3.up;
+            Vector3 right = lookRotation * -Vector3.right;
+            
+            //  Vector3 up = Vector3.Cross(forward, right);
+            mesh.SetVertices(new List<Vector3>(){
+                StartPoint + up + right,
+                StartPoint + -up + right,
+                StartPoint + up + -right,
+                StartPoint + -up + -right,
+                EndPoint + up + right,
+                EndPoint + -up + right,
+                EndPoint + up + -right,
+                EndPoint + -up + -right,
+            });
+            mesh.SetTriangles(new List<int>(){
+                2, 1, 0,
+                2, 3, 1,
+                4, 5, 6,
+                5, 7, 6,
+                0, 1, 4,
+                5, 4, 1,
+                6, 3, 2,
+                7, 3, 6,
+                4, 2, 0,
+                4, 6, 2,
+                1, 3, 5,
+                7, 5, 3,
+            }, 0);
+            filter.mesh = mesh;
+            if (SegmantType == ImpossibleSegementType.Spacer)
+            {
+                meshRenderer.sharedMaterial = SpacerMaterial;
+            }
+            else
+            {
+                meshRenderer.sharedMaterial = CastMaterial;
+            }
+           
+        }
+    }
+
+    void OnValidate()
+    {
+        BuildMesh();
+    }
+}
+
+
