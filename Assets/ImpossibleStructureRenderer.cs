360595ef323c51be883429d4ccc81163195b0631
ekeldevious <ender@shpazzle.com>
2017-11-06 21:01:59 -0500

    Resolved color normal bug

--- Assets/ImpossibleStructureRenderer.cs
+++ Assets/ImpossibleStructureRenderer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ImpossibleStructureRenderer : MonoBehaviour {
-
     public bool ShowOriginal;
     public ImpossibleStructure structure;
     public GameObject prefab;
@@ -12,7 +11,7 @@ public class ImpossibleStructureRenderer : MonoBehaviour {
     private Color UpColor = Color.blue;
     private Color RightColor = Color.red;
     private Color FowardColor = Color.green;
-    private List<GameObject> ObjectPool = new List<GameObject>();
+    private List<MeshFilter> ObjectPool = new List<MeshFilter>();
 
     // Use this for initialization
     void Start () {
@@ -24,14 +23,14 @@ public class ImpossibleStructureRenderer : MonoBehaviour {
         structure.AddSegment(new Vector3(10, 10, 10), Vector3.up);
         for (int i = 0; i < 6; i++)
         {
-            var prefabObject = Instantiate(prefab, Vector3.zero, Quaternion.identity);
-            ObjectPool.Add(prefabObject);
+            GameObject prefabObject = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            ObjectPool.Add(prefabObject.GetComponent<MeshFilter>());
         }
         BuildImpossibleStructure();
     }
 	
-	void Update () {
-        BuildImpossibleStructure();
+	void Update() {
+       BuildImpossibleStructure();
     }
 
     private void BuildImpossibleStructure()
@@ -39,35 +38,30 @@ public class ImpossibleStructureRenderer : MonoBehaviour {
         List<ImpossibleSegment> structureResult;
         if (ShowOriginal) structureResult = structure.GetSegments();
         else structureResult = structure.ProjectResult(Camera.main);
-        //List<CombineInstance> combineInstances = new List<CombineInstance>();
+        List<CombineInstance> combineInstances = new List<CombineInstance>();
         var count = 0;
         foreach (ImpossibleSegment segment in structureResult)
         {
-            // CombineInstance instance = new CombineInstance();
-           var mesh = BuildImpossibleSegmentMesh(segment);
-            // instance.transform = Matrix4x4.identity;
-            // combineInstances.Add(instance);
-            ObjectPool[count].GetComponent<MeshFilter>().mesh = mesh; 
-            count++;
+            CombineInstance instance = new CombineInstance();
+            var mesh = BuildImpossibleSegmentMesh(segment);
+            ObjectPool[count].mesh = mesh;
+            count += 1;
         }
         count = 0;
         for (int i = 0; i < structureResult.Count; i++)
         {
             var segment = structureResult[i];
-            //CombineInstance instance = new CombineInstance();
+            CombineInstance instance = new CombineInstance();
             var mesh = BuildImpossibleCorner(segment);
-            //instance.transform = Matrix4x4.identity;
-            //combineInstances.Add(instance);
-            ObjectPool[count+3].GetComponent<MeshFilter>().mesh = mesh;
+            ObjectPool[count + 3].mesh = mesh;
             count++;
         }
-        //filter.mesh.CombineMeshes(combineInstances.ToArray(), true);
     }
 
     private Mesh BuildImpossibleSegmentMesh(ImpossibleSegment segment)
     {
         Mesh mesh = new Mesh();
-        mesh.Clear();
+        mesh.MarkDynamic();
         Vector3 forward = -Vector3.Normalize(segment.End - segment.Start);
         Vector3 up = segment.Normal;
         Vector3 right = Vector3.Cross(up, forward);
@@ -103,7 +97,7 @@ public class ImpossibleStructureRenderer : MonoBehaviour {
     private Mesh BuildImpossibleCorner(ImpossibleSegment segment)
     {
             var mesh = new Mesh();
-            mesh.Clear();
+            mesh.MarkDynamic();
             Vector3 point = segment.Start;
             Vector3 forward = Vector3.Normalize(segment.End - segment.Start);
             Vector3 up = segment.Normal;
--- Assets/VertexColor.shader
+++ Assets/VertexColor.shader
@@ -1,24 +1,49 @@
-﻿Shader "Vertex color unlit" {
-	Properties{
-		_StencilMask("Stencil Mask", Int) = 1
-		_MainTex("Texture", 2D) = "white" {}
+﻿Shader "Unlit/VertexColored"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 100
+
+		Pass
+	{
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+
+#include "UnityCG.cginc"
+
+		struct appdata
+	{
+		float4 vertex : POSITION;
+		// Read vertex color from mesh.
+		fixed4 color : COLOR0;
+	};
+
+	struct v2f
+	{
+		// Pass interpolated vertex color to fragment.
+		fixed4 color : COLOR0;
+		float4 vertex : SV_POSITION;
+	};
+
+	v2f vert(appdata v)
+	{
+		v2f o;
+		o.vertex = UnityObjectToClipPos(v.vertex);
+		o.color = v.color; // Pass the color through.
+		return o;
 	}
 
-		Category {
-		Tags{ "Queue" = "Transparent+1" }
-		Lighting Off
-		BindChannels {
-			Bind "Color", color
-			Bind "Vertex", vertex
-			Bind "TexCoord", texcoord
-	    }
-
-		SubShader {
-			Pass  {
-				SetTexture[_MainTex] {
-					Combine texture * primary DOUBLE
-				}
-			}
-		}
+	fixed4 frag(v2f i) : SV_Target
+	{
+		// Output the color straight.
+		return i.color;
+	}
+		ENDCG
+	}
 	}
 }
