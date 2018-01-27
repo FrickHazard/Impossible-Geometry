ab99cc244bc5c9dddf91fb0eff5c6258a86085a4
ekeldevious <ender@shpazzle.com>
2018-01-26 20:32:24 -0500

    Cleaned up proj

---
+++ Assets/DynamicMesh.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicMesh : MonoBehaviour {
+
+    public bool Visible = true;
+
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Awake () {
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshRenderer = gameObject.AddComponent<MeshRenderer>();
+    }
+
+    public void SetVisible(bool visible)
+    {
+        meshRenderer.enabled = visible;
+    }
+
+    public void SetMesh(Mesh mesh)
+    {
+        meshFilter.mesh = mesh;
+    }
+
+    public void SetMaterial(Material material)
+    {
+        meshRenderer.material = material;
+    }
+	
+}
--- Assets/RectangularPrism.cs
+++ Assets/RectangularPrism.cs
@@ -10,12 +10,19 @@ public class RectangularPrism : MonoBehaviour {
     public float SegmentDistance;
     public Material material;
 
-    GameObject FrontWall;
-    GameObject BackWall;
-    GameObject RightWall;
-    GameObject LeftWall;
-    GameObject Ceiling;
-    GameObject Floor;
+    public bool ShowFrontWall;
+    public bool ShowBackWall;
+    public bool ShowRightWall;
+    public bool ShowLeftWall;
+    public bool ShowCeiling;
+    public bool ShowFloor;
+
+    DynamicMesh FrontWall;
+    DynamicMesh BackWall;
+    DynamicMesh RightWall;
+    DynamicMesh LeftWall;
+    DynamicMesh Ceiling;
+    DynamicMesh Floor;
 
 	void Start () {
         FrontWall = SetUpMeshObject("Front Wall");
@@ -28,34 +35,37 @@ public class RectangularPrism : MonoBehaviour {
 	
 	void Update () {
         Build();
-	}
+        UpdateVisibility();
+    }
+
+    public void UpdateVisibility()
+    {
+        FrontWall.SetVisible(ShowFrontWall);
+        BackWall.SetVisible(ShowBackWall);
+        RightWall.SetVisible(ShowRightWall);
+        LeftWall.SetVisible(ShowLeftWall);
+        Ceiling.SetVisible(ShowCeiling);
+        Floor.SetVisible(ShowFloor);
+    }
 
     void Build()
     {
-        Vector3 topOffset = Vector3.up * (Height / 2);
-        Vector3 rightOffset = Vector3.right * (Width / 2);
-        Vector3 forwardOffset = Vector3.forward * (Length / 2);
+       Vector3 topOffset = Vector3.up * (Height / 2);
+       Vector3 rightOffset = Vector3.right * (Width / 2);
+       Vector3 forwardOffset = Vector3.forward * (Length / 2);
 
-
-        Ceiling.GetComponent<MeshFilter>().mesh = SurfaceUtility.BuildRectangle(Vector3.zero + topOffset, topOffset + (Vector3.forward * Length), Width, SegmentDistance, -Vector3.up);
-
-        Floor.GetComponent<MeshFilter>().mesh = SurfaceUtility.BuildRectangle(Vector3.zero - topOffset, -topOffset + (Vector3.forward * Length), Width, SegmentDistance, Vector3.up);
-
-        RightWall.GetComponent<MeshFilter>().mesh = SurfaceUtility.BuildRectangle(Vector3.zero + rightOffset, rightOffset + (Vector3.forward * Length), Height, SegmentDistance, -Vector3.right);
-
-        LeftWall.GetComponent<MeshFilter>().mesh = SurfaceUtility.BuildRectangle(Vector3.zero - rightOffset, -rightOffset + (Vector3.forward * Length), Height, SegmentDistance, Vector3.right);
-
-        BackWall.GetComponent<MeshFilter>().mesh = SurfaceUtility.BuildRectangle(Vector3.zero - topOffset, Vector3.zero + (topOffset), Width, SegmentDistance, Vector3.forward);
-
-        FrontWall.GetComponent<MeshFilter>().mesh = SurfaceUtility.BuildRectangle((Vector3.forward * Length) - topOffset, (Vector3.forward * Length) + (topOffset), Width, SegmentDistance, -Vector3.forward);
-
+       FrontWall.SetMesh(SurfaceUtility.BuildRectangle((Vector3.forward * Length) - topOffset, (Vector3.forward * Length) + (topOffset), Width, SegmentDistance, -Vector3.forward));
+       BackWall.SetMesh(SurfaceUtility.BuildRectangle(Vector3.zero - topOffset, Vector3.zero + (topOffset), Width, SegmentDistance, Vector3.forward));
+       RightWall.SetMesh(SurfaceUtility.BuildRectangle(Vector3.zero + rightOffset, rightOffset + (Vector3.forward * Length), Height, SegmentDistance, -Vector3.right));
+       LeftWall.SetMesh(SurfaceUtility.BuildRectangle(Vector3.zero - rightOffset, -rightOffset + (Vector3.forward * Length), Height, SegmentDistance, Vector3.right));
+       Ceiling.SetMesh(SurfaceUtility.BuildRectangle(Vector3.zero + topOffset, topOffset + (Vector3.forward * Length), Width, SegmentDistance, -Vector3.up));
+       Floor.SetMesh(SurfaceUtility.BuildRectangle(Vector3.zero - topOffset, -topOffset + (Vector3.forward * Length), Width, SegmentDistance, Vector3.up));
     }
 
-    private GameObject SetUpMeshObject(string name)
+    private DynamicMesh SetUpMeshObject(string name)
     {
-        var result = new GameObject(name);
-        result.AddComponent<MeshFilter>();
-        result.AddComponent<MeshRenderer>().material = material;
+        var result = new GameObject(name).AddComponent<DynamicMesh>();
+        result.SetMaterial(material);
         result.transform.parent = transform;
         return result;
     }
