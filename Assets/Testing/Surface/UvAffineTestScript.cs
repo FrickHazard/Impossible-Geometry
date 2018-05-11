05429998643dc8e21493b38e0799af36b3864140
ekeldevious <ender@shpazzle.com>
2018-05-11 19:39:21 -0400

    Worked on more uv stuff

--- Assets/Testing/Surface/UvAffineTestScript.cs
+++ Assets/Testing/Surface/UvAffineTestScript.cs
@@ -2,56 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UvAffineTestScript : MonoBehaviour {
+public class UvAffineTestScript : MonoBehaviour
+{
 
-	void Start () {
+    void Start()
+    {
         MeshFilter filter = GetComponent<MeshFilter>();
         Mesh mesh = new Mesh();
-        mesh.SetVertices(new List<Vector3>(){
-           new Vector3(0f, 0f, -0.1f),
-           new Vector3(0.01f, 0f, 0f),
-           new Vector3(-0.01f, 0f, 0f),
-           new Vector3(0.1f, 0f, 0.1f),
-           new Vector3(-0.1f, 0f, 0.1f),
-           new Vector3(0.3f, 0f, 0.2f),
-           new Vector3(-0.3f, 0f, 0.2f),
+
+        PointData A = new PointData(
+            new Vector3(1f, 0f, 0f),
+            new Vector2(1f, 1f),
+            Vector3.up
+       );
+
+        PointData B = new PointData(
+            new Vector3(-1f, 0f, 0f),
+            new Vector2(0, 1f),
+            Vector3.up
+       );
+
+        PointData C = new PointData(
+           new Vector3(1f, 0f, 1f),
+           new Vector2(1f, 0f),
+           Vector3.up
+      );
+
+
+        PointData D = new PointData(
+            new Vector3(-1f, 0f, 1f),
+            new Vector2(0f, 0f),
+           Vector3.up
+       );
+
+
+
+        mesh.SetVertices(new List<Vector3>(){    
+            A.Point,
+            B.Point,
+            C.Point,
+
+            B.Point,
+            C.Point,
+            D.Point
         });
 
-        // distance ratio
-        float ratio = 0.1f / 0.3f;
-        float ratio2 = 0.01f / 0.3f;
-        mesh.SetUVs(0, new List<Vector4>(){
-           new Vector4(0.5f * 0f, 1f, 0f, 1f),
+        List<Vector4> uvs = new List<Vector4>() {
+            new Vector4(A.UVCoord.x, A.UVCoord.y, 1f, 1f),
+            new Vector4(B.UVCoord.x, B.UVCoord.y, 1f, 1f),
+            new Vector4(C.UVCoord.y * 0f, C.UVCoord.y, 0f, 1f),
 
-           new Vector4(0f * ratio2, 0.75f, ratio2, 1f),
-           new Vector4(1f * ratio2, 0.75f, ratio2 , 1f),
 
-           new Vector4(0f * ratio, 0.5f, ratio, 1f),
-           new Vector4(1f * ratio, 0.5f, ratio , 1f),
+            new Vector4(B.UVCoord.x * 0f, B.UVCoord.y, 0f, 1f),
+            new Vector4(C.UVCoord.x, C.UVCoord.y, 1f, 1f),
+            new Vector4(D.UVCoord.x, D.UVCoord.y, 1f, 1f),
+        };
 
-           new Vector4(0f, 0.25f, 1f, 1f),
-           new Vector4(1f, 0.25f, 1f, 1f),
-        });
-        mesh.SetNormals(new List<Vector3>(){
-           new Vector3(0, 1, 0),
-           new Vector3(0, 1, 0),
-           new Vector3(0, 1, 0),
-           new Vector3(0, 1, 0),
-           new Vector3(0, 1, 0),
-           new Vector3(0, 1, 0),
-           new Vector3(0, 1, 0),
-        });
+        mesh.SetUVs(0, uvs);
+
+        mesh.RecalculateNormals();
         mesh.RecalculateBounds();
         mesh.triangles = new int[] {
-            2, 1, 0,
-            1, 2, 3,
-            3, 2, 4,
-            5, 3, 4,
-            4, 6, 5 };
+          1, 2, 0,
+          5, 4, 3
+        };
         filter.mesh = mesh;
     }
-	
-	void Update () {
-		
-	}
+
+    void Update()
+    {
+
+    }
 }
---
+++ Assets/Testing/Surface/UvMapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AffineUvMapper {
+
+
+    public static Vector4 ProcessBezierSurfaceCoordinate(PointData reference, PointData rightPoint, PointData upPoint)
+    {
+        return new Vector4(0, 0, 0, 0);
+    }
+
+    public static List<Vector4> CalulateUVScaleRatio(PointData reference, PointData rightPoint, PointData upPoint)
+    {
+        float baseLength = Vector3.Distance(reference.Point, rightPoint.Point);
+        var result = new List<Vector4>();
+        var ratio = Vector3.Distance(reference.Point, upPoint.Point) / baseLength;
+        result.Add(new Vector4(reference.UVCoord.x, reference.UVCoord.y, 1f, 1f));
+        result.Add(new Vector4(rightPoint.UVCoord.x, rightPoint.UVCoord.y, 1f, 1f));
+        result.Add(new Vector4(upPoint.UVCoord.x * ratio, upPoint.UVCoord.y, ratio, 1f));
+        return result;
+    }
+
+}
